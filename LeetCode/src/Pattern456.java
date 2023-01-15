378ba3b4a6d1a6fa5b3079b16e594b8e1e46a3d6
sujit34 <sujitcsecuet@gmail.com>
2023-01-15 15:59:03 -0600

    456

--- LeetCode/src/Pattern456.java
+++ LeetCode/src/Pattern456.java
@@ -2,19 +2,19 @@ import java.util.*;
 
 public class Pattern456 {
 	public static boolean find132pattern(int[] nums) {
-		
+
 		Stack<Integer> stack = new Stack<Integer>();
 		Stack<Integer> minStack = new Stack<Integer>();
 		int curMin = nums[0];
 
 		for (int i = 0; i < nums.length; i++) {
 			while (!stack.isEmpty() && nums[i] >= stack.peek()) {
-				stack.pop();	
+				stack.pop();
 				minStack.pop();
 			}
 			if (!minStack.isEmpty() && nums[i] > minStack.peek())
 				return true;
-			
+
 			stack.push(nums[i]);
 			minStack.push(curMin);
 			if (curMin > nums[i])
@@ -23,9 +23,9 @@ public class Pattern456 {
 		return false;
 	}
 
-	public static void main(String[] args) {		
-		int arr[] = {3,1,4,2};
+	public static void main(String[] args) {
+		int arr[] = { 3, 1, 4, 2 };
 		System.out.print(find132pattern(arr));
-		
+
 	}
 }
