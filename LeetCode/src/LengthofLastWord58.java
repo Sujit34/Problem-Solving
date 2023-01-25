7b5a303baf3252928c244e27273b9ea1dd7e0d08
sujit34 <sujitcsecuet@gmail.com>
2023-01-25 17:08:01 -0600

    58

---
+++ LeetCode/src/LengthofLastWord58.java
@@ -0,0 +1,11 @@
+
+public class LengthofLastWord58 {
+	public static int lengthOfLastWord(String s) {
+		String[] str = s.split(" ");
+		return str[str.length - 1].length();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(lengthOfLastWord("Hello World"));
+	}
+}
