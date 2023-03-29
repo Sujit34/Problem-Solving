ca39071744a680cb64245ffcead5a953bb5d8f46
sujit34 <sujitcsecuet@gmail.com>
2023-03-28 22:29:08 -0500

    205

---
+++ LeetCode/src/IsomorphicStrings205.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class IsomorphicStrings205 {
+	public static boolean isIsomorphic(String s, String t) {
+		HashMap<Character, Character> hashMapst = new HashMap<Character, Character>();
+		HashMap<Character, Character> hashMapts = new HashMap<Character, Character>();
+
+		for (int i = 0; i < s.length(); i++) {
+			if (hashMapst.containsKey(s.charAt(i))) {
+				if (hashMapst.get(s.charAt(i)) != t.charAt(i))
+					return false;
+
+			} else {
+				hashMapst.put(s.charAt(i), t.charAt(i));
+			}
+
+			if (hashMapts.containsKey(t.charAt(i))) {
+				if (hashMapts.get(t.charAt(i)) != s.charAt(i))
+					return false;
+
+			} else {
+				hashMapts.put(t.charAt(i), s.charAt(i));
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isIsomorphic("egg", "add"));
+		System.out.println(isIsomorphic("foo", "bar"));
+		System.out.println(isIsomorphic("paper", "title"));
+		System.out.println(isIsomorphic("badc", "baba"));
+	}
+}
