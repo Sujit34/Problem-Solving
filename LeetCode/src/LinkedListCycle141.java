f3dedfc3f34b413604caaeed3c7e6c0dbeab3b29
sujit34 <sujitcsecuet@gmail.com>
2023-04-11 21:14:26 -0500

    141

--- LeetCode/src/LinkedListCycle141.java
+++ LeetCode/src/LinkedListCycle141.java
@@ -25,7 +25,7 @@ public class LinkedListCycle141 {
     }
 	
 	public static void main(String[] args) {	
-		ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5,new ListNode(6))))));		
+		ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5,new ListNode(6,new ListNode(7)))))));		
 		System.out.print(hasCycle(l1));	
 	}
 
