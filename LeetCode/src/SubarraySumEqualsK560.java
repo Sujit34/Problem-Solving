aefc58df30a9b3c7211a6a7b5dbcb554681deb11
sujit34 <sujitcsecuet@gmail.com>
2023-04-15 21:40:30 -0500

    560

---
+++ LeetCode/src/SubarraySumEqualsK560.java
@@ -0,0 +1,14 @@
+
+public class SubarraySumEqualsK560 {
+	public static int subarraySum(int[] nums, int k) {
+		int result =0;
+		
+		return result;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 5,-5,5,-5 };
+		System.out.println(subarraySum(arr, 0));
+	}
+
+}
