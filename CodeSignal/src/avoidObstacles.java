b23c92292deee4af446d91836d1fede505dfb1ff
sujit34 <sujitcsecuet@gmail.com>
2023-05-31 00:19:14 -0500

    avoidObstacles

--- CodeSignal/src/avoidObstacles.java
+++ CodeSignal/src/avoidObstacles.java
@@ -1,20 +1,31 @@
 import java.util.Arrays;
 
 public class avoidObstacles {
-	int solution(int[] inputArray) {
+	static int solution(int[] inputArray) {
 
 		Arrays.sort(inputArray);
 
 		int max = inputArray[inputArray.length - 1];
 
 		for (int i = 2; i < max; i++) {
+			boolean flag = false;
+			;
 			for (int j = 0; j < inputArray.length; j++) {
-				if (inputArray[i] % 2 == 0) {
-
+				if (inputArray[j] % i == 0) {
+					flag = true;
+					break;
 				}
 			}
+			if (flag == false)
+				return i;
 		}
 
-		return res;
+		return max + 1;
+	}
+
+	public static void main(String[] args) {
+		// int[] arr = {5, 3, 6, 7, 9};
+		int[] arr = { 2, 3 };
+		System.out.println(solution(arr));
 	}
 }
